91bd2257243098f5f03e73d70c2c2987c28ac5bc
game <game@R200-18>
2025-09-03 12:24:29 +0900

    ボス行動変更

--- 3D_Base/Source/GameObject/StaticMeshObject/CCharacter/CBoss/CBoss.cpp
+++ 3D_Base/Source/GameObject/StaticMeshObject/CCharacter/CBoss/CBoss.cpp
@@ -42,7 +42,7 @@ void CBoss::Update()
 			case AttackSequenceState::Jump:
 				m_bossAttackManager->CreateBossAttack(CBossAttackManager::BossAttackList::Jump, GetPosition(), playerPos, m_pPlayer);
 				m_fSlashCoolTime = 0.f;
-				m_eCurrentAttackSequenceState = AttackSequenceState::Charge;
+				m_eCurrentAttackSequenceState = AttackSequenceState::Slash;
 				break;
 
 			case AttackSequenceState::Slash:
